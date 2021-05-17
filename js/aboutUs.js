9b41007843872d08802394fcde087c4be8443b4f
alahundraa <33134480+alahundraa@users.noreply.github.com>
2021-05-17 22:01:23 +0300

    commit

---
+++ js/aboutUs.js
@@ -0,0 +1,1 @@
+function initMap(){const our_office={lat:55.79225274647401,lng:49.12247575167198};const map=new google.maps.Map(document.getElementById("map"),{center:our_office,zoom:15,});const marker=new google.maps.Marker({position:our_office,map:map,})}
